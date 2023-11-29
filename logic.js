144772b1c6f4564502a4745a7d4fac9d1823f709
nadeen-maree <nadeen.maree1610@gmail.com>
2023-11-29 09:14:54 +0200

    dummy.js file added

--- logic.js
+++ logic.js
@@ -7,26 +7,7 @@ const NUM_INDEXS_TO_SPLICE = 1
 
 const Tweeter = function() {
 
-    const _posts = [
-        {
-            text: "First post!",
-            id: "p1",
-            comments: [
-                { id: "c1", text: "First comment on first post!" },
-                { id: "c2", text: "Second comment on first post!!" },
-                { id: "c3", text: "Third comment on first post!!!" }
-            ]
-        },
-        {
-            text: "Aw man, I wanted to be first",
-            id: "p2",
-            comments: [
-                { id: "c4", text: "Don't wory second poster, you'll be first one day." },
-                { id: "c5", text: "Yeah, believe in yourself!" },
-                { id: "c6", text: "Haha second place what a joke." }
-            ]
-        }
-    ]
+    const _posts = posts
 
     let postIdCounter = INITIAL_POST_ID
     let commentIdCounter = INITIAL_COMMENT_ID
